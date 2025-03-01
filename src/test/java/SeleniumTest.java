a251067b367243776bd1afad67d4af576713e200
ThangCua17 <thangpvph52569@gmail.com>
2025-03-01 18:38:57 +0700

    update

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -22,18 +22,27 @@ public class SeleniumTest {
     }
     @Test
     public void test() {
-        driver.get("https://vitimex.com.vn/");
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        WebElement btnIcon = driver.findElement(By.xpath("//div[@class='li_log']"));
-        btnIcon.click();
-        WebElement inpSDT = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='account']")));
-        inpSDT.sendKeys("0987194692");
-        WebElement inpPass = driver.findElement(By.xpath("//input[@id='pass']"));
-        inpPass.sendKeys("123456");
-        WebElement btnDN = driver.findElement(By.xpath("//a[@id='signin-btn']"));
-        btnDN.click();
+//        driver.get("https://vitimex.com.vn/");
+//
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//
+//        WebElement btnIcon = driver.findElement(By.xpath("//div[@class='li_log']"));
+//        btnIcon.click();
+//        WebElement inpSDT = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='account']")));
+//        inpSDT.sendKeys("0987194692");
+//        WebElement inpPass = driver.findElement(By.xpath("//input[@id='pass']"));
+//        inpPass.sendKeys("123456");
+//        WebElement btnDN = driver.findElement(By.xpath("//a[@id='signin-btn']"));
+//        btnDN.click();
+        //Them vao gio hang
+        driver.get("https://vitimex.com.vn/ao-polo-pc14.html");
+        Actions action = new Actions(driver);
+        WebElement btnAnh = driver.findElement(By.xpath("//a[7]//div[1]//img[2]"));
+       action.moveToElement(btnAnh).click().perform();
+        WebElement btnSize = driver.findElement(By.xpath("//a[@id='size4']"));
+        btnSize.click();
+        WebElement btnGH = driver.findElement(By.xpath("//a[@id='addCart_page0']"));
+        action.moveToElement(btnGH).click().perform();
 
 
 
